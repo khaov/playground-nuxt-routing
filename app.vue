55ffc4282e4882aba8ca6f9f1d2994ccba725d34
Artyon Khaov <a.khaov@gmail.com>
2024-11-22 13:25:13 +0700

    feat: creating AppNavigation component with Routing

--- app.vue
+++ app.vue
@@ -1,5 +1,46 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="page">
+    <header class="header">
+      Хедер
+    </header>
+    <aside class="aside">
+      <AppNavigation />
+    </aside>
+    <main class="content">
+      <div>
+      <NuxtPage />
+    </div>
+    </main>
+    <footer class="footer">
+      Футер
+    </footer>
   </div>
 </template>
+
+<style>
+  .page{
+    display: grid;
+
+    grid-template-areas:
+    "header header"
+    "aside content"
+    "footer footer";
+  }
+
+  .header {
+    grid-area: header;
+  }
+
+  .content {
+    grid-area: content;
+  }
+
+  .aside {
+    grid-area: aside;
+  }
+
+  .footer {
+    grid-area: footer;
+  }
+
+</style>
---
+++ components/AppNavigation.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const router = useRouter()
+  const routes = router.getRoutes();
+</script>
+
+<template>
+  <ul>
+    <li v-for="route in routes" :key="route.path">
+      <NuxtLink :to="route.path" >
+        {{ route.name }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
